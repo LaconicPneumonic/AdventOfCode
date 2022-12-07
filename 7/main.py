55bc91a9d3ac8ea208a44286736027151d919686
Anthony Rolland <6172844+LaconicPneumonic@users.noreply.github.com>
2022-12-07 16:58:45 -0500

    Reduce Day 7 Complexity

--- 7/main.py
+++ 7/main.py
@@ -1,10 +1,12 @@
 from typing import List
 from collections import Counter
-FILE_NAME = './input.txt'
+
+FILE_NAME = "./input.txt"
+
 
 def problem():
 
-    with open(FILE_NAME, 'r') as f:
+    with open(FILE_NAME, "r") as f:
 
         stack = ["/"]
         f.readline()
@@ -15,47 +17,60 @@ def problem():
             line = f.readline().strip()
             tokens: List[str] = line.strip().split(" ")
 
-            if tokens[0] == '$' and tokens[1] == 'cd':
-                if tokens[2] != '..':
+            if tokens[0] == "$" and tokens[1] == "cd":
+                if tokens[2] != "..":
                     # a folder is defined by its current PWD
-                    currPath = stack[-1] + "/" +  tokens[2]
+                    currPath = stack[-1] + "/" + tokens[2]
                     print("ENTERING", currPath)
                     stack.append(currPath)
                 else:
                     # we're leaving this folder maybe to return
                     leaving = stack.pop()
-                    print("LEAVING", leaving)
+                    directorySizes[stack[-1]] += directorySizes[leaving]
+                    print(
+                        f"LEAVING  {leaving} FOR {stack[-1]} AND ADDING {directorySizes[leaving]}"
+                    )
             elif tokens[0].isnumeric():
                 # we've found a file, so add the size to every path we've seen so far
                 print("FILE", f"{stack[-1] + '/' +  tokens[1]}:{tokens[0]}")
-                for directory in stack:
-                    directorySizes[directory] += int(tokens[0])
-            elif tokens[0] == '':
+                directorySizes[stack[-1]] += int(tokens[0])
+
+            elif tokens[0] == "":
                 # end of file remove "/"
-                stack.pop()
+                leaving = stack.pop()
+                if stack:
+                    directorySizes[stack[-1]] += directorySizes[leaving]
+                    print(
+                        f"LEAVING  {leaving} FOR {stack[-1]} AND ADDING {directorySizes[leaving]}"
+                    )
 
             else:
                 # we are ignoring 'dir *' and 'ls *' since they do not contribute to our flow
                 pass
-    
 
         print()
         # problem 1
-        print(f"SPACE OF DIRS <= 100000 = {sum([value for _, value in directorySizes.items() if value <= 100000])}")
-        
+        print(f"CURR SIZE {directorySizes['/']}")
+        print(
+            f"SPACE OF DIRS <= 100000 = {sum([value for _, value in directorySizes.items() if value <= 100000])}"
+        )
+
         # problem 2
         TOTAL = 70000000
         spaceNeeded = 30000000 - (TOTAL - directorySizes["/"])
         print(f"SPACE NEEDED {spaceNeeded}")
-        minimumDirectory = float('inf')
+        minimumDirectory = float("inf")
         minDirectoryName = ""
         for d, size in directorySizes.items():
-            if size >= spaceNeeded:
-                minimumDirectory = min(minimumDirectory, size)
+            if size >= spaceNeeded and size <= minimumDirectory:
+                minimumDirectory = size
                 minDirectoryName = d
-        print(f"FILE TO REMOVE{minDirectoryName}:{minimumDirectory}")
+
+        print(f"FILE TO REMOVE {minDirectoryName} => {minimumDirectory}")
+
 
 def main():
-   problem()
+    problem()
+
 
 main()
